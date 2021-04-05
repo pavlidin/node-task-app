88f3c70fb9c33d102a506f49ea02849139b6c2f4
Nikos Pavlidis <nikospavlidismail@gmail.com>
2021-04-06 02:08:09 +0300

    add sorting, pagination, filtering

--- src/models/user.js
+++ src/models/user.js
@@ -2,61 +2,68 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const Task = require('./task')
+const Task = require('./task');
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid!');
-            }
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
         },
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number!');
-            }
-        },
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password should not contain word "password".');
-            }
-        },
-    },
-    tokens: [
-        {
-            token: {
-                type: String,
-                required: true,
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Email is invalid!');
+                }
             },
         },
-    ],
-});
+        age: {
+            type: Number,
+            default: 0,
+            validate(value) {
+                if (value < 0) {
+                    throw new Error('Age must be a positive number!');
+                }
+            },
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 7,
+            trim: true,
+            validate(value) {
+                if (value.toLowerCase().includes('password')) {
+                    throw new Error(
+                        'Password should not contain word "password".'
+                    );
+                }
+            },
+        },
+        tokens: [
+            {
+                token: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
 
 userSchema.virtual('tasks', {
     ref: 'Task',
     localField: '_id',
-    foreignField: 'owner'
-})
+    foreignField: 'owner',
+});
 
 userSchema.methods.toJSON = function () {
     const user = this;
@@ -108,9 +115,9 @@ userSchema.pre('save', async function (next) {
 // Delete user tasks when user is removed
 userSchema.pre('remove', async function (next) {
     const user = this;
-    await Task.deleteMany({ owner: user._id })
-    next()
-})
+    await Task.deleteMany({ owner: user._id });
+    next();
+});
 
 const User = mongoose.model('User', userSchema);
 
--- src/routers/task.js
+++ src/routers/task.js
@@ -3,10 +3,35 @@ const router = new express.Router();
 const auth = require('../middleware/auth');
 const Task = require('../models/task');
 
+// Filter - GET /tasks?completed=true
+// Pagination GET /tasks?limit=10&skip=0
+// Sorting - GET /tasks?sortBy=createdAt:asc
 router.get('/tasks', auth, async (req, res) => {
+    const match = {};
+    const sort = {};
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true';
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+    }
+
     try {
         //const tasks = await Task.find({ owner: req.user._id })
-        await req.user.populate('tasks').execPopulate();
+        await req.user
+            .populate({
+                path: 'tasks',
+                match,
+                options: {
+                    limit: parseInt(req.query.limit),
+                    skip: parseInt(req.query.skip),
+                    sort,
+                },
+            })
+            .execPopulate();
         res.send(req.user.tasks);
     } catch (e) {
         res.status(500).send();
@@ -25,7 +50,10 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     }
 
     try {
-        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+        const task = await Task.findOne({
+            _id: req.params.id,
+            owner: req.user._id,
+        });
 
         if (!task) {
             return res.status(404).send();
@@ -43,7 +71,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
 
     try {
-        const task = await Task.findOne({ _id, owner: req.user._id })
+        const task = await Task.findOne({ _id, owner: req.user._id });
 
         if (!task) {
             return res.status(404).send();
@@ -58,8 +86,8 @@ router.get('/tasks/:id', auth, async (req, res) => {
 router.post('/tasks', auth, async (req, res) => {
     const task = new Task({
         ...req.body,
-        owner: req.user._id
-    })
+        owner: req.user._id,
+    });
 
     try {
         await task.save();
@@ -70,9 +98,11 @@ router.post('/tasks', auth, async (req, res) => {
 });
 
 router.delete('/tasks/:id', auth, async (req, res) => {
-
     try {
-        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
+        const task = await Task.findOneAndDelete({
+            _id: req.params.id,
+            owner: req.user._id,
+        });
 
         if (!task) {
             return res.status(404).send();
